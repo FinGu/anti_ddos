9809e8b060d1cd12b48d110b3a7fed59cdfa589b
Fabio <50637180+Finguimbrine@users.noreply.github.com>
2020-07-11 16:02:48 -0300

    kekw

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include("funcs/include.php");
 if(verify\check_and_create() != general\responses::success)
     die("!");
 
-$handler2 = new reverse_proxy($_SERVER['REQUEST_URI'], "http://localhost/test.php", "deps/reverse_proxy.php", false);
+$handler = new reverse_proxy($_SERVER['REQUEST_URI'], "http://localhost/test.php", "deps/reverse_proxy.php", false);
 
-$handler2->execute();
+$handler->execute();
 
 ?>
